a44906ec067aed8221a6242cc44c127c6690b20b
Hamoud-Limam <hamoudlimam01@gmail.com>
2024-10-16 09:11:44 -0700

    Add files via upload

---
+++ src/compoments/videos.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function Videos(){
+const [videos,setVideos]= useState([]);
+
+useEffect(()=>{
+    async function getBooks(){
+  try{
+    const data = await fetch('/api/getvideos');
+    const allVideos = await data.json()
+    setVideos(allVideos)
+    console.log()
+  }
+  
+  catch(err){
+    console.log(err)
+  }
+    }
+    getBooks()
+},[])
+
+return (
+  <> <div dir="rtl" className="flex overflow-x-scroll scroll-snap-x mandatory -webkit-overflow-scrolling-touch p-2 mr-5 transition-transform duration-300 ease-in-out">
+    {videos.map(video => (
+     
+        <div key={video._id} className=" flex-none relative w-[13vw] h-auto scroll-snap-start mx-[-2px] transform scale-[0.9] transition-transform duration-300 ease-in-out hover:scale-90">
+          <a href={video.url}>
+            <img className="flex-none relative w-[13vw] h-auto scroll-snap-start mx-[-2px] transform scale-[0.9] transition-transform duration-300 ease-in-out hover:scale-100" src={video.imgUrl} alt={video.title} />
+          </a>
+        </div>
+    ))}
+    
+    </div>
+  </>
+);
+
+}
+
